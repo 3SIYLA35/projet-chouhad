4a08abf6b1ebe992fea9ff5792705d08cce158f9
rachidaarag <rachidaraq108@gmail.com>
2024-04-23 00:24:13 +0100

    modification1

--- shape.h
+++ shape.h
@@ -1,6 +1,28 @@
 #ifndef shape
 #define shape
-#include "rendu1.cpp"
+#include <iostream>
+ using namespace std;
+
+typedef struct S2d {double x=0.; double y=0.;}base ;
+class segment{
+    private:
+    base point;
+    float angle;
+    float longueur;
+    public:
+    segment(float poix,float poiy,float ang,float lon)
+    :angle(ang),longueur(lon),point(poix,poiy){};
+    float getangle(){return angle;}
+    float getlongueur(){return longueur;}
+    void affiche(){
+    }
+    float extr_x(){
+        return point.x+longueur*cos(angle);
+    };
+    float extr_y(){
+        return point.y+longueur*sin(angle);
+    };
+};
 
 
-#endif /* FED8B4D8_C30F_4B7C_9BD1_554D1D82E4CD */
+#endif 
