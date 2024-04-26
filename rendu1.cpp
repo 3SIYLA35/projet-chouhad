39a944313a21298bcee88107656e12e6aadd6b5e
rachidaarag <rachidaraq108@gmail.com>
2024-04-26 23:39:44 +0100

    nhar 1 (orientation)

--- rendu1.cpp
+++ rendu1.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
-#include "shape.h"
 #include <cmath>
 using namespace std;
+
+typedef struct S2d {double x=0.; double y=0.;}base ;
+class segment{
+    private:
+    base point;
+    float angle;
+    float longueur;
+    public:
+    segment(float poix,float poiy,float ang,float lon)
+    :angle(ang),longueur(lon){point.x=poix;
+    point.y=poiy;};
+    float getangle(){return angle;}
+    float getlongueur(){return longueur;}
+    double getX(){return point.x;}
+    double getY(){return point.y;}
+    void affiche(){
+    }
+    base extr(){
+        double x=point.x+longueur*cos(angle);
+        double y=point.y+longueur*sin(angle);
+        return {x,y};
+    };
+    base get_base(){return point;}
+}; 
--- shape.h
+++ shape.h
@@ -1,39 +1,34 @@
 #ifndef shape
 #define shape
 #include <iostream>
+#include "rendu1.cpp"
 #include <cmath>
 #include "PI.H"
+#define EPSILON 1e-9
  using namespace std;
 
-typedef struct S2d {double x=0.; double y=0.;}base ;
-class segment{
-    private:
-    base point;
-    float angle;
-    float longueur;
-    public:
-    segment(float poix,float poiy,float ang,float lon)
-    :angle(ang),longueur(lon),point(poix,poiy){};
-    float getangle(){return angle;}
-    float getlongueur(){return longueur;}
-    void affiche(){
-    }
-    base extr(){
-        double x=point.x+longueur*cos(angle);
-        double y=point.y+longueur*sin(angle);
-        return {x,y};
-    };
-    float get_angle(){return angle;}
-    base get_base(){return point;}
+
+double ecart_angulair( segment& segment1, segment& segment2) {
+    base end1 = segment1.extr();
+    base end2 = segment2.extr();
+    double angle1=atan2(end1.y-segment1.getY(), end1.x-segment1.getX());
+    double angle2=atan2(end2.y-segment2.getY(),end2.x-segment2.getX());
+     double angular = angle2 - angle1;
+    while(angular>Pi){angular-=2*Pi;}
+    while (angular<=-Pi){angular+=2*Pi;};
+    return angular;
 };
-float ecart_angulaire( segment & A,segment& B){
-        float diff=A.get_angle()-B.get_angle();
-        if(diff>Pi)
-            diff=2*Pi-diff;
-        else
-            diff=2*Pi+diff;
-        return diff;
-    }
+bool super_pos( segment& segment1,  segment& segment2) {
+    double angularGap = ecart_angulair(segment1, segment2);
+    return fabs(angularGap) < EPSILON;
+}
+int orientation(base p,base q,base r) 
+{
+    int val=(q.y-p.y)*(r.x-q.x)-(q.x-p.x)*(r.y-q.y); 
+    if (val==0) return 0;
+  
+    return (val > 0)? 1: 2; 
+}
 bool intersection(segment& A,segment& B){
         float prod_vectoriel1 = (B.extr().x-A.get_base().x)*(A.get_base().y-B.get_base().y)-
                                 (A.get_base().x-B.get_base().x)*(B.extr().y-A.get_base().y);
